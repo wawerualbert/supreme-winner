5c21304e1bbc5f5a98d5c346dde40e9474ca9d4f
wawerualbert58 <wawerualbert58@gmail.com>
2020-09-06 19:27:14 -0400

    update to heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/dist/angular-r'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-r/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
